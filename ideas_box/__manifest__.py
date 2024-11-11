0726892511fe7df2ffcd31357b7496d2de560adf
pantxo-artzai <f.patissier@hodei.net>
2024-11-11 18:33:03 +0100

    module idea box

---
+++ ideas_box/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ideas Box',
+    'version': '18.0.1.0',
+    'category': 'Project',
+    'description': u"""
+This module enables efficient management of ideas within a company by providing a centralized "Ideas Box" for submitting, categorizing, and tracking ideas from team members, fostering a culture of innovation and collaboration.
+""",
+    'author': 'Franck PATISSIER',
+    'depends': [
+        'web_grid',
+    ],
+    'data': [
+        'data/ir_model.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_ui_view.xml',
+        'data/ir_actions_act_window.xml',
+        'data/ir_ui_menu.xml',
+        'data/ir_model_access.xml',
+        'data/ir_default.xml',
+    ],
+    'license': 'OPL-1',
+}
